eace5bbdaca256b7814ab034b23cbc55dc3cdf9b
Hendrik Leppkes <h.leppkes@gmail.com>
2014-05-12 08:35:06 +0200

    w32pthreads: add return values to pthread_join and pthread_cond_init to match posix

--- compat/w32pthreads.h
+++ compat/w32pthreads.h
@@ -92,14 +92,15 @@ static int pthread_create(pthread_t *thread, const void *unused_attr,
     return !thread->handle;
 }
 
-static void pthread_join(pthread_t thread, void **value_ptr)
+static int pthread_join(pthread_t thread, void **value_ptr)
 {
     DWORD ret = WaitForSingleObject(thread.handle, INFINITE);
     if (ret != WAIT_OBJECT_0)
-        return;
+        return EINVAL;
     if (value_ptr)
         *value_ptr = thread.ret;
     CloseHandle(thread.handle);
+    return 0;
 }
 
 static inline int pthread_mutex_init(pthread_mutex_t *m, void* attr)
@@ -156,6 +157,7 @@ static int pthread_cond_init(pthread_cond_t *cond, const void *unused_attr)
 
     pthread_mutex_init(&win32_cond->mtx_waiter_count, NULL);
     pthread_mutex_init(&win32_cond->mtx_broadcast, NULL);
+
     return 0;
 }
 
