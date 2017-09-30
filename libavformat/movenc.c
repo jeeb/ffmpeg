ba96154e3e1b3607e8958330abda2cfd46466537
Jan Ekström <jeebjp@gmail.com>
2017-09-30 17:57:02 +0300

    movenc: switch the ISMV tfxd fragment timestamp to be DTS
    As far as can be seen, this value is supposed to be the DTS of a
    fragment in smooth streaming. Thus, don't take the initial sample's
    CTS offset into mention when calculating the start timestamp.
    
    This can be especially seen with H.264 with b-frames in smooth
    streaming, where implementations will first parse the txfd
    timestamp, and then once more add the CTS offset of the first
    sample to that timestamp to get the presentation time of that
    sample, leading to even more A/V desync and/or actual
    discontinuities when the DTS+(2*CTS offset)+duration value
    ends up larger than the following tfxd's start timestamp.
    
    The duration calculation requires PTS values, so it is not touched.

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -4096,8 +4096,7 @@ static int mov_write_tfxd_tag(AVIOContext *pb, MOVTrack *track)
     avio_write(pb, uuid, sizeof(uuid));
     avio_w8(pb, 1);
     avio_wb24(pb, 0);
-    avio_wb64(pb, track->start_dts + track->frag_start +
-                  track->cluster[0].cts);
+    avio_wb64(pb, track->start_dts + track->frag_start);
     avio_wb64(pb, track->end_pts -
                   (track->cluster[0].dts + track->cluster[0].cts));
 
