eda23b378246e618914a81ff31e16b4520e33af0
Jan Ekström <jan.ekstrom@24i.com>
2022-07-22 13:57:54 +0300

    ffmpeg: add video heartbeat capability to fix_sub_duration
    Splits the currently handled subtitle at random access point
    packets that can be configured to follow a specific output stream.
    Currently only subtitle streams which are directly mapped into the
    same output in which the heartbeat stream resides are affected.
    
    This way the subtitle - which is known to be shown at this time
    can be split and passed to muxer before its full duration is
    yet known. This is also a drawback, as this essentially outputs
    multiple subtitles from a single input subtitle that continues
    over multiple random access points. Thus this feature should not
    be utilized in cases where subtitle output latency does not matter.
    
    Co-authored-by: Andrzej Nadachowski <andrzej.nadachowski@24i.com>
    Co-authored-by: Bernard Boulay <bernard.boulay@24i.com>
    
    Signed-off-by: Jan Ekström <jan.ekstrom@24i.com>

--- fftools/ffmpeg.h
+++ fftools/ffmpeg.h
@@ -224,6 +224,8 @@ typedef struct OptionsContext {
     int        nb_reinit_filters;
     SpecifierOpt *fix_sub_duration;
     int        nb_fix_sub_duration;
+    SpecifierOpt *fix_sub_duration_heartbeat;
+    int        nb_fix_sub_duration_heartbeat;
     SpecifierOpt *canvas_sizes;
     int        nb_canvas_sizes;
     SpecifierOpt *pass;
@@ -625,6 +627,12 @@ typedef struct OutputStream {
 
     int sq_idx_encode;
     int sq_idx_mux;
+
+    /*
+     * bool on whether this stream should be utilized for splitting
+     * subtitles utilizing fix_sub_duration at random access points.
+     */
+    unsigned int fix_sub_duration_heartbeat;
 } OutputStream;
 
 typedef struct OutputFile {
