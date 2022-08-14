6cb87487a5c2fc4a05490d2b9af96f2a12d5fd33
Jan Ekström <jeebjp@gmail.com>
2022-08-14 18:39:03 +0300

    lavfi/libplacebo: add COBJMACROS define

--- libavfilter/vf_libplacebo.c
+++ libavfilter/vf_libplacebo.c
@@ -16,6 +16,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#define COBJMACROS
+
 #include "libavutil/file.h"
 #include "libavutil/hwcontext_d3d11va.h"
 #include "libavutil/opt.h"
