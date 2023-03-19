91adc55fe8fe51a424f31f73cb41b78e42526b23
Jan Ekström <jeebjp@gmail.com>
2023-03-19 15:37:40 +0200

    avutil/frame: add helper for getting side data from array

--- libavutil/frame.h
+++ libavutil/frame.h
@@ -1101,6 +1101,20 @@ int av_frame_side_data_from_sd(AVFrameSideData ***sd, int *nb_sd,
                                unsigned int flags);
 
 /**
+ * Get a side data entry of a specific type from a set.
+ *
+ * @param sd    array of side data.
+ * @param nb_sd integer containing the number of entries in the array.
+ * @param type  type of side data to be queried
+ *
+ * @return a pointer to the side data of a given type on success, NULL if there
+ *         is no side data with such type in this set.
+ */
+const AVFrameSideData *av_frame_side_data_get(const AVFrameSideData **sd,
+                                              const int nb_sd,
+                                              enum AVFrameSideDataType type);
+
+/**
  * @}
  */
 
